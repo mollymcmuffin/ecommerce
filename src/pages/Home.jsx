36502b3f65f1458898daf6a328ba382e45da670c
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-07-13 16:24:08 +0800

    Added Footer,Newsletter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Annoucement from "../components/Annoucement";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const home = () => {
   return (
@@ -12,6 +14,8 @@ const home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
+      <Footer />
     </>
   );
 };
